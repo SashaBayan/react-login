4060bbdd384f976f6e5be65052cb777d67babc14
Sasha Bayan <sashabayan@gmail.com>
2015-10-27 21:25:22 -0700

    feat: set up boiler-plate routing

--- main.jsx
+++ main.jsx
@@ -4,6 +4,61 @@ if (typeof window !== 'undefined') {
 }
 
 import React from 'react';
-import { App } from './components/App.jsx';
+import { render } from 'react-dom';
+import { Router, Route, Link } from 'react-router'
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group'
+import { createHistory, useBasename } from 'history'
 
-React.render(<App />, document.getElementById("app"));
+import './components/styles/app.css';
+// import { App } from './components/App.jsx';
+
+const history = useBasename(createHistory)({
+  basename: ''
+})
+
+class App extends React.Component {
+  render() {
+    const { pathname } = this.props.location
+    return (
+      <div>
+        <ul>
+          <li><Link to="/page1">Page 1</Link></li>
+          <li><Link to="/page2">Page 2</Link></li>
+        </ul>
+        {React.cloneElement(this.props.children || <div />, { key: pathname })}
+      </div>
+    )
+  }
+}
+
+// render(<App />, document.getElementById("app"));
+
+class Page1 extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Page 1</h1>
+      </div>
+    )
+  }
+}
+
+class Page2 extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Page 2</h1>
+      </div>
+    )
+  }
+}
+
+
+render((
+  <Router history={history}>
+    <Route path="/" component={App}>
+      <Route path="page1" component={Page1} />
+      <Route path="page2" component={Page2} />
+    </Route>
+  </Router>
+), document.getElementById('app'))
