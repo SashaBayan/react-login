806183dc517b3254abb8220eef9d425d5eaf0058
Sasha Bayan <sashabayan@gmail.com>
2015-11-01 22:13:00 -0800

    chore: update scripts and add linter for optimized dev env

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
-var express = require('express');  
-var app = express();  
-var port = process.env.PORT || 3000;  
-app.listen(port);
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Your server is listening on localhost:${port}`));
 
 app.use("/", express.static(__dirname));
 
-app.get('/', function(req, res){  
+app.get('/', function(req, res){
   res.render(__dirname + 'index.html');
-}); 
+});
+
