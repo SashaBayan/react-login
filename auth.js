27b37a36ff23d05d0d89fcb33dc34e4e9f86b605
Sasha Bayan <sashabayan@gmail.com>
2015-11-09 18:13:20 -0800

    refactor: create separate check for username

--- auth.js
+++ auth.js
@@ -1,8 +1,12 @@
 import registeredUsers from './registeredUsers';
 
-export default function authenticate(username, password) {
+export function checkUserExists(username) {
+  return registeredUsers.hasOwnProperty(username);
+}
+
+export function authenticate(username, password) {
   // traverse object to see if there is a username match
-  if (registeredUsers[username]) {
+  if (checkUserExists(username)) {
     // check if password for that user matched input password
     return registeredUsers[username].password === password;
   }
